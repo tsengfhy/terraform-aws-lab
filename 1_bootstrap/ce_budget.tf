c2824589a82e14272c2f97895dcb4cdd0de9e7fa
Tsengfhy <tsengfhy@gmail.com>
2022-10-17 13:38:04 +0000

    use bandwidth usage budget

--- 1_bootstrap/ce_budget.tf
+++ 1_bootstrap/ce_budget.tf
@@ -7,11 +7,38 @@ resource "aws_budgets_budget" "monthly_cost" {
 
   dynamic "notification" {
     for_each = length(var.notification_email_addresses) > 0 ? [0] : []
-    
+
     content {
       comparison_operator        = "GREATER_THAN"
       threshold_type             = "PERCENTAGE"
       threshold                  = "90"
+      notification_type          = "ACTUAL"
+      subscriber_email_addresses = var.notification_email_addresses
+    }
+  }
+}
+
+resource "aws_budgets_budget" "bandwidth_usage" {
+  name         = "${local.prefix}budget-bandwidth-usage"
+  budget_type  = "USAGE"
+  time_unit    = "MONTHLY"
+  limit_unit   = "GB"
+  limit_amount = "100"
+
+  cost_filter {
+    name = "UsageType"
+    values = [
+      "${join("", [for item in split("-", "ap-northeast-1") : upper(substr(item, 0, length(item) == 2 ? 2 : 1))])}-DataTransfer-Out-Bytes",
+    ]
+  }
+
+  dynamic "notification" {
+    for_each = length(var.notification_email_addresses) > 0 ? [0] : []
+
+    content {
+      comparison_operator        = "GREATER_THAN"
+      threshold_type             = "PERCENTAGE"
+      threshold                  = "100"
       notification_type          = "ACTUAL"
       subscriber_email_addresses = var.notification_email_addresses
     }
