75846189a855656941ae8205ac0ee9d8cfcd4a3a
Tsengfhy <tsengfhy@gmail.com>
2023-08-28 23:17:41 +0800

    update run.sh

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -2,6 +2,6 @@
 
 export WORKSPACE=global
 
-terraform init -backend-config=../backend-config.tfvars
+terraform init --backend-config=../backend-config.tfvars
 terraform workspace select $WORKSPACE || terraform workspace new $WORKSPACE
-terraform plan -var-file=../common.tfvars -lock=false
+terraform plan --var-file=../common.tfvars -lock=false
--- stack/run.sh
+++ stack/run.sh
@@ -2,6 +2,6 @@
 
 export WORKSPACE=dev
 
-terraform init -backend-config=../backend-config.tfvars
+terraform init --backend-config=../backend-config.tfvars
 terraform workspace select $WORKSPACE || terraform workspace new $WORKSPACE
-terraform plan -var-file=../common.tfvars -lock=false
+terraform plan --var-file=../common.tfvars -lock=false
